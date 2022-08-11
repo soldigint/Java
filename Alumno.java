f38c3f9c83fff112d9a7028396daa72e3536c506
Laura Patricia Ramirez Rivera <97982013+soldigint@users.noreply.github.com>
2022-08-11 12:51:37 -0500

    Create Alumno.java

---
+++ Alumno.java
@@ -0,0 +1,30 @@
+package Mochila;
+
+public class Alumno {
+String Nombre;
+int [] ArregloPagos;
+Boleta boleta;
+Curso curso;
+
+Alumno (String n)
+{
+	Nombre=n;
+	}
+
+boolean TomarClase(boolean a )
+{
+	return a;
+	}
+boolean RecibirBoleta(boolean b)
+{
+	return b;
+	}
+boolean EntregarTarea(boolean b)
+{
+	return b;
+	}
+boolean Pagar(boolean p)
+{
+	return p;
+	}
+}
